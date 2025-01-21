506a58c459be8c4b861340837a56ff9f06c371c1
jhowmodderxxxx <heverttonrodrigozin112@gmail.com>
2025-01-21 11:00:30 -0300

    Update haxstorescriptPC.lua

--- haxstorescriptPC.lua
+++ haxstorescriptPC.lua
@@ -53,7 +53,7 @@ local DeviceType = game:GetService("UserInputService").TouchEnabled and "Mobile"
     TextButton.Size = UDim2.new(0, 45, 0, 45)
     TextButton.AutoButtonColor = false
     TextButton.Font = Enum.Font.SourceSans
-    TextButton.Text = "Open"
+    TextButton.Image = "rbxassetid://115939816657500"
     TextButton.TextColor3 = Color3.new(220, 125, 255)
     TextButton.TextSize = 20
 
