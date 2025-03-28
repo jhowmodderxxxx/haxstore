15334d0d5da60bc463b0fbda0ac54bcb62e1b437
jhowmodderxxxx <heverttonrodrigozin112@gmail.com>
2025-03-28 11:06:40 -0300

    Update sowxteam.lua

--- sowxteam.lua
+++ sowxteam.lua
@@ -98,7 +98,7 @@ else
 end
 
 
-gg.alert("VERSAO V2 CORRIGIDA \n\n Bug de script terminado corrigida \n nova funçao (tela esticada) \n mais otimizacaono script \n bypass ant kick implementado")
+gg.alert("VERSAO V2 CORRIGIDA \n\n Bug de script terminado foi corrigida \n nova funçao (tela esticada) \n mais otimização no script \n bypass ant kick implementado")
 
 
 versao = "SCRIPT SOWxTEAM V2 PREMIUM"
